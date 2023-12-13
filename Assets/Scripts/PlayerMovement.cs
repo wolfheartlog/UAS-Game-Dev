d4038c2b769934a3840d1d1b23fb9703ed63b6ab
minxs33 <theminxs12@gmail.com>
2023-12-13 13:06:32 +0700

    [add] Adding yuliana & mom's assets + partial character animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        Vector2 dir = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+            animator.SetFloat("Direction", 1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+            animator.SetFloat("Direction", 2);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.y = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            dir.y = -1;
+        }
+
+        dir.Normalize();
+        animator.SetBool("isMoving", dir.magnitude > 0);
+
+        GetComponent<Rigidbody2D>().velocity = speed * dir;
+    }
+}
+
